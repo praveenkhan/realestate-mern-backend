5201a2f87773a15b3a1e0348ee38045c9e806974
PRABHA <praveenprabha387@gmail.com>
2026-02-28 23:21:02 +0530

    update backend

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -23,8 +23,8 @@ export const createContact = async (req, res) => {
     // 2️⃣ Create transporter (Gmail SMTP)
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
